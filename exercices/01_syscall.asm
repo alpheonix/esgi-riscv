dd8b277f3ac7336450ebc7a4ac5baaaa5feb8e47
alpheonix <arthurblanc98@gmail.com>
2020-03-25 09:47:46 +0100

    Update 01_syscall.asm

--- exercices/01_syscall.asm
+++ exercices/01_syscall.asm
@@ -19,8 +19,13 @@ message: .asciz "Hello ASM from .data"
 # Exercice: Observation dans Rars
 #
 # - Quelle est l'addresse de base de la chaine msg? Son addresse de fin?
+#   -> addresse de base: 0x10010000
+#   -> addresse de fin:  0x10010014
 # - Dans quelle segment de la mémoire est elle?
+#   -> dans .data
 # - Quelle sont les valeurs en hexadecimales présente dans .data?
+#   -> 0x6c6c6548  0x6f57206f  0x20646c72  0x6d6f7266  0x61642e20  0x00006174
+#   -> .data: 2e 64 61 74 61
 
 # la: Load Address, charge une adresse (label) dans un registre
 la a0, ??? # argument de l'appel systeme
